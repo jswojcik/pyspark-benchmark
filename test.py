8d5e49596dbe9c5924e749ff1e65f9ab4f0f22b6
jswojcik <jswojcik9@gmail.com>
2022-11-26 11:52:53 -0700

    mac

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from timeit import default_timer as timer
 from pyspark import SparkContext
 import random
 
-N = 100_000_000_000
+N = 100_000_0000
 start_time = timer()
 def sample(p):
     x, y = random.random(), random.random()
     return 1 if x*x + y*y < 1 else 0
 
-sc = SparkContext("local[*]", "Test App")
+sc = SparkContext("local", "Test App")
 count = sc.parallelize(range(0, N)).map(sample).reduce(lambda a, b: a + b)
 end_time = timer()
 result = end_time-start_time
